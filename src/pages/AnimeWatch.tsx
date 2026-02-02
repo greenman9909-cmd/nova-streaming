b2da24bc70cdfeb3da255ff6b26cf80c8c2d5d50
Nova Builder <nova@builder.local>
2026-02-02 02:07:35 +0100

    Fix: Remove unused import to fix build (Final)

--- src/pages/AnimeWatch.tsx
+++ src/pages/AnimeWatch.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useParams, useSearchParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import {
     getAnimeInfo,
     getAnimeEpisodes,
